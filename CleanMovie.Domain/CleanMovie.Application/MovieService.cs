c8651f73a2016ca0ad00ecf7676ef8517fe2112f
ayman.abdellatief <ayman.abdellatief@it-blocks.com>
2023-09-24 14:59:19 +0200

    Clean Architecture

---
+++ CleanMovie.Domain/CleanMovie.Application/MovieService.cs
@@ -0,0 +1,25 @@
+﻿using CleanMovie.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovie.Application
+{
+    public class MovieService : IMovieService
+    {
+        private readonly IMoveRepository _moveRepository;
+
+        public MovieService(IMoveRepository moveRepository)
+        {
+            _moveRepository = moveRepository;
+        }
+        public List<Movie> GetAllMovies()
+        {
+            var Movies = _moveRepository.GetAllMovies();
+
+            return Movies;
+        }
+    }
+}
